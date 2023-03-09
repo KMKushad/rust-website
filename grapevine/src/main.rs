615dc933e9dff64a5461bc384498f882ba355027
KMKushad <kmkushad@gmail.com>
2023-03-08 21:50:37 -0500

    added static file support and templating support

--- grapevine/src/main.rs
+++ grapevine/src/main.rs
@@ -1,58 +1,41 @@
 #[macro_use] extern crate rocket;
 
-use rocket::tokio::time::{sleep, Duration};
-use rocket::fs::NamedFile;
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
-use rocket_dyn_templates::{Template, tera::Tera, context};
+//use rocket::response::content::RawHtml;
+use rocket::fs::{FileServer, relative};
+
+//use rocket::Request;
+//use rocket::response::Redirect;
+
+use rocket_dyn_templates::{Template, context};
+
+use std::io;
 
 #[get("/")]
 fn index() -> Template {
-    Template::render("index", context! {
+    Template::render("hello", context! {
         title: "Hello"
     })
 }
 
-#[get("/delay/<seconds>")]
-async fn delay(seconds: u64) -> String {
-    sleep(Duration::from_secs(seconds)).await;
-    format!("Waited for {} seconds", seconds)
-}
+#[get("/profile")]
+fn profile() -> Template {
+    let mut username = String::new();
 
-#[get("/greet/<name>")]
-fn greet(name: &str) -> String {
-    format!("Hello, {}", name)
-}
+    io::stdin()
+        .read_line(&mut username)
+        .expect("Failed to read line");
 
-#[get("/upload")]
-fn upload() -> String {
-    let path = Path::new("test.txt");
-    let display = path.display();
-
-    let mut file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", display, why),
-        Ok(file) => file,
-    };
-
-    let mut s = String::new();
-    match file.read_to_string(&mut s) {
-        Err(why) => panic!("couldn't open {}: {}", display, why),
-        Ok(_) => (),
-    };
-
-    s
-}
-
-#[get("/<file..>")]
-async fn files(file: std::path::PathBuf) -> Option<NamedFile> {
-    NamedFile::open(std::path::Path::new("static/").join(file)).await.ok()
+    Template::render("profile", context! {
+        username,
+        email: "test@gmail.com"
+    })
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![files, index])
-        .mount("/other_routes", routes![index, delay, greet, upload])
+        .attach(Template::fairing())
+        .mount("/static", FileServer::from(relative!("static")))
+        .mount("/", routes![index, profile])
 }
 
